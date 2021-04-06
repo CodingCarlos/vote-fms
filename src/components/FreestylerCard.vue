211c5ff6b20e2d52d610842d47b8fa3726af3757
CodingCarlos <hola@codingcarlos.com>
2021-04-06 20:19:10 +0200

    Primeros toques

---
+++ src/components/FreestylerCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="freestyler-card">
+    <img :src="pic" />
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FreestylerCard',
+  props: {
+    pic: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.freestyler-card {
+  display: flex;
+  flex-direction: column;
+  max-width: 200px;
+  padding: 8px;
+  border: 1px solid black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FreestylerCard
+      name="Manolo Lama"
+      pic="https://picsum.photos/200"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import FreestylerCard from '@/components/FreestylerCard.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    FreestylerCard,
   },
 };
 </script>
